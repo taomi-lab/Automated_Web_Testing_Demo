e02551dad5e7c08e5944189cd4cf7bb76c5aa550
TaoMi <81752269+taomi-lab@users.noreply.github.com>
2024-08-04 17:08:33 +0800

    update

---
+++ paths_manager.py
@@ -0,0 +1,9 @@
+import os
+
+# 获取当前文件所在目录，其实就是项目根目录
+project_path = os.path.dirname(__file__)
+login_json = f"{project_path}/config/login.json"
+buytickes_json = f"{project_path}/config/buy_tickets.json"
+screenshot_path = f"{project_path}/images"
+
+
---
+++ run.py
@@ -0,0 +1,9 @@
+# !/usr/bin python3
+# encoding: utf-8 -*-
+# @file     : run.py
+# @author   : TaoMi
+# @Time     : 2024/7/30 下午3:55
+import pytest
+
+if __name__ == '__main__':
+    pytest.main()
